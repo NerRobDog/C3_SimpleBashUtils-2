439e3c33f71df80c0451b622c4eda33ab00a676f
Ernest Sharie <ernestsh@student.21-school.ru>
2023-11-03 16:06:24 +0300

    v0.5

---
+++ src/grep/s21_grep.c
@@ -0,0 +1,99 @@
+#include "s21_grep.h"
+
+int main(int count, char *buffer[]) {
+    struct Flags flag = {0, 0, 0, 0, 0, 0};
+    char pattern_buffer[BUFFER_SIZE];
+    //
+    grepFlags(count, buffer, &flag);
+    return 0;
+}
+
+bool grepFlags(int count, char *buffer[], flag *Flags) {
+    int flag_value;
+
+    while ((flag_value = getopt_long(count, buffer, NULL)) != -1) {
+        switch (flag_value) {
+            case 'e':
+                Flags -> flag_e = 1;
+                break;
+            case 'i':
+                Flags -> flag_i = 1;
+                break;
+            case 'v':
+                Flags -> flag_v = 1;
+                break;
+            case 'c':
+                Flags -> flag_c = 1;
+                break;
+            case 'l':
+                Flags -> flag_l = 1;
+                break;
+            case 'n':
+                Flags -> flag_n = 1;
+                break;
+            default:
+                printf("ERROR: invalid arguments\n");
+                break;
+        }
+        //if (Flags -> flag_b && Flags -> flag_n) Flags -> flag_n  = 0;
+    }
+    return 0;
+}
+
+/**
+ *  * поиск во всех файлах
+ *  -e шаблон
+ * -i поиск без учета регистра
+ *  -w поиск всего слова
+ *  -c кол-во совпадений
+ *  -r поиск в подкаталогах
+ *  -i инверсия
+ *  -n номера строк
+*/
+
+int printer(char *buffer[], flag *Flags) {
+    FILE *file;
+    file = fopen(buffer[optind], "r");
+    if (file != NULL) {
+        int str_count = 1;
+        bool empty_str_count = 0;
+        int last = '\n';
+        while (!feof(file)) {
+            int c = fgetc(file); // считывает данные с потока и делает из них строку
+            if (Flags -> flag_s && c == '\n' && last == '\n') {
+                empty_str_count++;
+                if (empty_str_count > 1) {
+                    continue;
+                }
+            } else {
+                empty_str_count = 0;
+            }
+            if (last == '\n' && ((Flags -> flag_b && c != '\n') ||Flags -> flag_n)) {
+                printf("%6d  ", str_count++);
+            }
+            if (Flags -> flag_t && c == '\t') {
+                printf("^");
+                c = 'I';
+            }
+            if (Flags -> flag_e && c == '\n')
+                printf("$");
+            if (Flags -> flag_v) {
+                if ((c >= 0 && c < 9) || (c > 10 && c < 32) || (c > 126 && c <= 160)) {
+                    printf("^");
+                    if (c > 126) {
+                        c -= 64;
+                    } else {
+                        c += 64;
+                    }
+                }
+            }
+            printf("%c", c);
+            last = c;
+        }
+        fclose(file);
+        printf("\n");
+    } else {
+        printf("ERROR: incorrect file name\n");
+        return 1;
+    }
+}
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -1,19 +1,23 @@
-#ifndef SRC_CAT_S21_CAT_H_
-#define SRC_CAT_S21_CAT_H_
+#ifndef SRC_GREP_S21_GREP_H_
+#define SRC_GREP_S21_GREP_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "string.h"
 #include <unistd.h>
 #include <stdbool.h>
 #include <getopt.h>
+#include "<regex.h>"
+
+#define int BUFFER_SIZE 4096;
 
 typedef struct Flags {
-    bool flag_b;
     bool flag_e;
-    bool flag_n;
-    bool flag_s;
-    bool flag_t;
+    bool flag_i;
     bool flag_v;
+    bool flag_c;
+    bool flag_l;
+    bool flag_n;
 } flag;
 
 struct option options[] = { // библиотека getopt
@@ -23,7 +27,7 @@ struct option options[] = { // библиотека getopt
         {0, 0, 0, 0},
 };
 
-bool catFlags(int count, char *buffer[], flag *Flags);
+bool grepFlags(int count, char *buffer[], flag *Flags);
 int printer(char *buffer[], flag *Flags);
 
-#endif // SRC_CAT_S21_CAT_H_
+#endif // SRC_GREP_S21_GREP_H_
