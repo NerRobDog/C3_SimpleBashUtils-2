24271eae34ec4e89434ed55f55e0785ea8f48b4d
Ernest Sharie <ernestsh@student.21-school.ru>
2023-11-09 22:36:59 +0300

    v0.6

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,22 +1,25 @@
 #include "s21_grep.h"
 
-int main(int count, char *buffer[]) {
+int main(int count, char* buffer[]) {
     struct Flags flag = {0, 0, 0, 0, 0, 0};
     char pattern_buffer[BUFFER_SIZE] = {0};
 
-    grepFlags(count, buffer, &flag, pattern_buffer);
+    grep_flags(count, buffer, &flag, pattern_buffer);
+    if (count >= 3) {
+        grep_launcher(&flag, count, buffer, pattern_buffer);
+    }
     return 0;
 }
 
-bool grepFlags(int count, char *buffer[], flag *Flags, char *pattern_buffer) {
-    bool flag_value; // будет ли работать с bool
-    char* f_options = "eivcln";
+void grep_flags(int count, char *buffer[], flag *Flags, char *pattern_buffer) {
+    int flag_value;
+    char* f_options = "eivcln"; // отредачить
 
-    while ((flag_value = getopt_long(count, buffer, f_options)) != -1) {
+    while ((flag_value = getopt(count, buffer, f_options)) != -1) {
         switch (flag_value) {
             case 'e':
                 Flags -> flag_e = 1;
-                snprintf(pattern_buffer, BUFFER_SIZE, "%s", optarg)// getopt(3) external variables
+                snprintf(pattern_buffer, BUFFER_SIZE, "%s", optarg);  // getopt(3) external variables
                 break;
             case 'i':
                 Flags -> flag_i = 1;
@@ -37,69 +40,103 @@ bool grepFlags(int count, char *buffer[], flag *Flags, char *pattern_buffer) {
                 printf("ERROR: invalid arguments\n");
                 break;
         }
-        //if (Flags -> flag_b && Flags -> flag_n) Flags -> flag_n  = 0;
     }
-    return 0;
 }
 
-/**
- *  * поиск во всех файлах
- *  -e шаблон
- * -i поиск без учета регистра
- *  -w поиск всего слова
- *  -c кол-во совпадений
- *  -r поиск в подкаталогах
- *  -i инверсия
- *  -n номера строк
-*/
-
-void grep_trigger() {
-
-}
-
-int printer(char *buffer[], flag *Flags) {
-    FILE *file;
-    file = fopen(buffer[optind], "r");
-    if (file != NULL) {
-        int str_count = 1;
-        bool empty_str_count = 0;
-        int last = '\n';
-        while (!feof(file)) {
-            int c = fgetc(file); // считывает данные с потока и делает из них строку
-            if (Flags -> flag_s && c == '\n' && last == '\n') {
-                empty_str_count++;
-                if (empty_str_count > 1) {
-                    continue;
+void grep_launcher(flag *Flags, int count, char *buffer[], char *buff) {
+    char pattern[BUFFER_SIZE] = {NULL}; // пустой шаблон
+    int end_of_pattern = 0;
+    if (!Flags -> flag_f && !Flags -> flag_e) { // если нет шаблона
+        snprintf(pattern, BUFFER_SIZE, "%s", buffer[optind++]);
+    }
+    if (Flags -> flag_f) { // регулярки (press F)
+        end_of_pattern = grep_f_flag(pattern, buff); // вызов функции
+    }
+    if (!Flags -> flag_f && Flags -> flag_e) { // шаблон без регулярки
+        snprintf(pattern, BUFFER_SIZE, "%s", buff);
+    }
+    if (end_of_pattern != -1) { //если без ошибок
+        int file_count = 0;
+        if (count - optind > 1) { // optind - индекс текущего параметра
+            file_count = 1;
+            for (int i = optind; i < count; i++) {
+                if (file_count && !Flags -> flag_h && !Flags -> flag_l) { // если файл есть
+                    printf("%s:", buffer[i]);
                 }
-            } else {
-                empty_str_count = 0;
+                grep_file(Flags, pattern, buffer[i]);
             }
-            if (last == '\n' && ((Flags -> flag_b && c != '\n') ||Flags -> flag_n)) {
-                printf("%6d  ", str_count++);
-            }
-            if (Flags -> flag_t && c == '\t') {
-                printf("^");
-                c = 'I';
-            }
-            if (Flags -> flag_e && c == '\n')
-                printf("$");
-            if (Flags -> flag_v) {
-                if ((c >= 0 && c < 9) || (c > 10 && c < 32) || (c > 126 && c <= 160)) {
-                    printf("^");
-                    if (c > 126) {
-                        c -= 64;
-                    } else {
-                        c += 64;
-                    }
-                }
-            }
-            printf("%c", c);
-            last = c;
         }
-        fclose(file);
-        printf("\n");
-    } else {
-        printf("ERROR: incorrect file name\n");
-        return 1;
     }
+}
+
+void grep_file(flag *Flags, char *pattern, char *file_name) {
+    int cflags = REG_EXTENDED; // расш. регулярные выражения 🏆🥇
+    regex_t reg; // f
+    FILE *file;
+    file = fopen(file_name, "r");
+    if (Flags -> flag_i) {
+        cflags = REG_ICASE; // не учитывать регистр 🥈
+    }
+    if (file != NULL) {
+        regcomp(&reg, pattern, cflags); // обработка рег. выр.
+        grep_process(Flags, file, reg, file_name);
+        regfree(&reg); // высвобождение
+        fclose(file);
+    }
+}
+
+void grep_process(flag *Flags, FILE *file, regex_t reg, char *file_name) {
+    char text[BUFFER_SIZE] = {0};
+    regmatch_t pmatch[1];
+    int line_matches = 0, line_number = 1;
+
+    while (fgets(text, BUFFER_SIZE - 1, file) != NULL) { // считывает данные с потока и делает из них строку
+        bool match = 0;
+        bool success = regexec(&reg, text, 1, pmatch, 0);  // успешность поиска слова       может не bool
+
+        if (strchr(text, '\n') == NULL) // порядковый номер (с единицы)
+            strcat(text, "\n");
+        if (success == 0 && !Flags -> flag_v)
+            match = 1;
+        if (success == REG_NOMATCH && Flags -> flag_v)
+            match = 1;
+        if (match && !Flags -> flag_l && !Flags -> flag_c && Flags -> flag_n)
+            printf("%d:", line_number); // нумерует все строки
+        if (match && !Flags -> flag_l && !Flags -> flag_c && !Flags -> flag_o)
+            printf("%s", text);  // просто выводит совпадение
+        if (match && Flags -> flag_o) {
+            for (int i = pmatch[0].rm_so; i < pmatch[0].rm_eo; i++) { // выводит нужный кусок строки (совпадение)
+                printf("%c", text[i]);
+            }
+            printf("\n");
+        }
+        line_matches += (int)match;
+        line_number++;
+    }
+    if (Flags -> flag_l && line_matches > 0)
+        printf("%s\n", file_name);
+    if (Flags -> flag_c && !Flags -> flag_l)
+        printf("%d\n", line_matches);
+}
+
+int grep_f_flag(char *pattern, char *file_name) {
+    FILE *file;
+    file = fopen(file_name, "r");
+    int i = 0;
+    if (file != NULL) {
+        int ch;
+        while ((ch = fgetc(file)) != EOF) {
+            if (ch == 13 || ch == 10)
+                pattern[i++] = '|';
+            if (ch != 13 && ch != 10)
+                pattern[i++] = ch;
+        }
+    } else {
+        printf("error\n");
+        i = -1;
+    }
+    if (pattern[i - 1] == '|')
+        pattern[i - 1] = '\0';
+    fclose(file);
+    return i;
 }
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -7,9 +7,9 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <getopt.h>
-#include "<regex.h>"
+#include "regex.h"
 
-#define int BUFFER_SIZE 2048;
+#define BUFFER_SIZE 4096
 
 typedef struct Flags {
     bool flag_e;
@@ -18,11 +18,16 @@ typedef struct Flags {
     bool flag_c;
     bool flag_l;
     bool flag_n;
+    bool flag_h;
+    bool flag_s;
+    bool flag_f;
+    bool flag_o;
 } flag;
 
-bool grepFlags(int count, char *buffer[], flag *Flags, char *pattern_buffer);
-int printer(char *buffer[], flag *Flags);
-void grep_trigger();
-
+void grep_flags(int count, char *buffer[], flag *Flags, char *pattern_buffer);
+void grep_launcher(flag *Flags, int count, char *buffer[], char *buff);
+void grep_file(flag *Flags, char *pattern, char *file_name);
+void grep_process(flag *Flags, FILE *file, regex_t reg, char *file_name);
+int grep_f_flag(char *pattern, char *file_name);
 
 #endif // SRC_GREP_S21_GREP_H_
