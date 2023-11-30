4b790c59ef4b4d657bc98a8bdd70e7005b3acf19
Ernest Sharie <ernestsh@student.21-schoo.ru>
2023-11-30 20:51:10 +0300

    v1(Clanged .c and .h, fixed Makefile)

--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,11 +1,11 @@
 #ifndef SRC_CAT_S21_CAT_H_
 #define SRC_CAT_S21_CAT_H_
 
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <getopt.h>
 
 typedef struct Flags {
     bool flag_b;
@@ -16,14 +16,15 @@ typedef struct Flags {
     bool flag_v;
 } flag;
 
-struct option options[] = { // библиотека getopt
-        {"number-nonblank", 0, 0, 'b'},
-        {"number", 0, 0, 'n'},
-        {"squeeze-blank", 0, 0, 's'},
-        {0, 0, 0, 0},
+struct option options[] = {
+    // библиотека getopt
+    {"number-nonblank", 0, 0, 'b'},
+    {"number", 0, 0, 'n'},
+    {"squeeze-blank", 0, 0, 's'},
+    {0, 0, 0, 0},
 };
 
 bool catFlags(int count, char *buffer[], flag *Flags);
 int printer(char *buffer[], flag *Flags);
 
-#endif // SRC_CAT_S21_CAT_H_
+#endif  // SRC_CAT_S21_CAT_H_
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -1,13 +1,13 @@
 #ifndef SRC_GREP_S21_GREP_H_
 #define SRC_GREP_S21_GREP_H_
 
+#include <getopt.h>
+#include <regex.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <getopt.h>
-#include <regex.h>
 
 #define BUFFER_SIZE 4096
 
@@ -30,4 +30,4 @@ void grep_file(flag *Flags, char *pattern, char *file_name);
 void grep_process(flag *Flags, FILE *file, regex_t reg, char *file_name);
 int grep_f_flag(char *pattern, char *file_name);
 
-#endif // SRC_GREP_S21_GREP_H_
+#endif  // SRC_GREP_S21_GREP_H_
